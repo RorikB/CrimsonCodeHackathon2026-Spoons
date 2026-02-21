391819466f0c98caf00c04dc9232945160dc35c9
NomadicCat <55854868+NomadicCat@users.noreply.github.com>
2026-02-21 15:07:28 -0800

    gemini test work

--- gemini.py
+++ gemini.py
@@ -1,7 +1,10 @@
 from google import genai
+import os
+from dotenv import load_dotenv
 
-# client = genai.Client(api_key="YOUR_API_KEY")
-
+# Load the environment variables from the .env file
+load_dotenv()
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 print("Client initialized successfully.")
 response = client.models.generate_content(
     model="gemini-3-flash-preview", contents="Explain how AI works in a few words"

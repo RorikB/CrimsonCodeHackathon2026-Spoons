1e0a0e3bf67da7860a688d063bdd2c1f37a43a4d
Lorelew3 <lorelew3@gmail.com>
2026-02-21 17:36:41 -0800

    moved main.py and edited layout and render

--- modules/gui/layout.py
+++ modules/gui/layout.py
@@ -1,6 +1,5 @@
 def get_layout_zones(screen_width, screen_height):
-    return 
-    {
+    return {
         "Time": (50,50,100,100),
         "Weather": (screen_width - 350, 50, 300, 100),
         "Calendar": (50,200,500,300),
--- modules/gui/render.py
+++ modules/gui/render.py
@@ -1,5 +1,6 @@
 import cv2
-from datetime import dateTime, datetime
+from datetime import datetime
+from .layout import get_layout_zones
 
 def render_zones(frame, zones, state):
     for name, (x,y,w,h) in zones.items():
@@ -12,6 +13,6 @@ def render_zones(frame, zones, state):
         else:
             content = state.get(name, f"{name} Placeholder")
 
-            # Draw text
-            cv2.putText(frame, str(content), (x+10, y+30), cv2.Font_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+        # Draw text
+        cv2.putText(frame, str(content), (x+10, y+30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
     return frame
